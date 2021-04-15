5ff482245cd9aab965202a95d9450afbd9999209
Shaggy <simmons5family@gmail.com>
2021-04-14 18:43:50 -0700

    add header component to app

---
+++ portfolio/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+          <div>Logo</div>
+          <nav>
+            <ul className='nav-list'>
+              <li>Home</li>
+              <li>Projects</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </header>
+    )
+}
+
+export default Header
