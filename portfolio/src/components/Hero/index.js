30d9e1c42f5c0953045c6cea8c7b75e996edb8d3
Shaggy <simmons5family@gmail.com>
2021-04-14 18:59:48 -0700

    basic hero component

--- portfolio/src/components/Hero/index.js
+++ portfolio/src/components/Hero/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import img from '../../assets/images/green-code-typing.png';
 
 const Hero = () => {
     return (
-        <div>
-          
+        <div className='hero-handler'>
+          <div className='hero-text-handler'>
+            <h2>Some Hero Title</h2>
+            <p>
+              hero text to go below the hero Title
+            </p>
+          </div>
+          <div className='hero-img-handler'>
+            <img className='hero-img' src={img} alt='green web dev computer'></img>
+          </div>
         </div>
     )
 }
