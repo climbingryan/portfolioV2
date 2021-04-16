cebfa1b2aed9e371f573b58b3aebb79a43460562
Shaggy <simmons5family@gmail.com>
2021-04-15 21:40:02 -0700

    modify Hero & Projects components, add pizza-hunt project, add waves at top and bottom of Project component

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -9,6 +9,18 @@ const Projects = () => {
       class: "collectors-haven",
     },
     {
+      title: "Collectors Haven",
+      description: "This site was built using MERN stack along with apollo boost, GraphQl for the API, react-router-dom, and more. I worked on the UI & UX trying to make a responsive, but clean and intuitive ui. ",
+      link: "https://aqueous-taiga-86315.herokuapp.com/",
+      class: "collectors-haven",
+    },
+    {
+      title: "Budget Tracker",
+      description: "This was a challenge where i was to convert an existing app into a pwa. Included is a web manifest, service worker to handle offline functionality, and indexDb to store offline data for when the user goes back online",
+      link: "https://safe-oasis-51981.herokuapp.com/",
+      class: "pwa",
+    },
+    {
       title: "Budget Tracker",
       description: "This was a challenge where i was to convert an existing app into a pwa. Included is a web manifest, service worker to handle offline functionality, and indexDb to store offline data for when the user goes back online",
       link: "https://safe-oasis-51981.herokuapp.com/",
@@ -16,8 +28,8 @@ const Projects = () => {
     },
     {
       title: "Pizza Hunt",
-      description: "This was a challenge where i was to convert an existing app into a pwa. Included is a web manifest, service worker to handle offline functionality, and indexDb to store offline data for when the user goes back online",
-      link: "sleepy-shore-05861.herokuapp.com/",
+      description: "This was my first mongodb fullstack application built as a lesson during my bootcamp. Using Node.js, Mongodb, mongoose",
+      link: "https://boiling-ravine-77749.herokuapp.com/",
       class: "pizza-hunt",
     },
   ];
@@ -31,7 +43,12 @@ const Projects = () => {
               <h3>{project.title}</h3>
               <p>
                 {project.description}{" "}
-                <a href={project.link} target="_blank">
+                <a
+                  className="accent"
+                  href={project.link}
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   Click Here
                 </a>
               </p>
@@ -39,6 +56,13 @@ const Projects = () => {
           </div>
         );
       })}
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#E5DEF7"
+          fill-opacity="1"
+          d="M0,288L48,272C96,256,192,224,288,224C384,224,480,256,576,272C672,288,768,288,864,282.7C960,277,1056,267,1152,240C1248,213,1344,171,1392,149.3L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+        ></path>
+      </svg>
     </div>
   );
 };
